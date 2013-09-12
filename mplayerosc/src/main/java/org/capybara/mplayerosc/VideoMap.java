7110551054371469c29c89a73dcab45fd04c897a
dfraser <dfraser@capybara.org>
2013-09-12 13:34:12 -0400

    sort the directory list by filename, ignoring case

--- mplayerosc/src/main/java/org/capybara/mplayerosc/VideoMap.java
+++ mplayerosc/src/main/java/org/capybara/mplayerosc/VideoMap.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
@@ -25,15 +28,26 @@ public class VideoMap {
 
 	public void updateMap() throws IOException {
 		videoList.clear();
-		int i = 1;
+		List<Path> files = new ArrayList<>();
 		try (DirectoryStream<Path> stream =
 		     Files.newDirectoryStream(path, "*.{avi,jpg,png,mov,mpg,mp4}")) {
-		    for (Path entry: stream) {
-		    	String videoName = FilenameUtils.removeExtension(entry.getFileName().toString());
-		    	videoList.put(i, new Video(videoName, 	entry, i));
-		    	i++;
+			for (Path entry: stream) {
+				files.add(entry);
+			}
+		}
+		
+		Collections.sort(files, new Comparator<Path>() {
+		    public int compare(Path o1, Path o2) {
+		            return o1.getFileName().toString().compareToIgnoreCase(o2.getFileName().toString());
 		    }
-		} 
+		});
+
+		int i = 1;
+		for (Path entry: files) {
+			String videoName = FilenameUtils.removeExtension(entry.getFileName().toString());
+			videoList.put(i, new Video(videoName, 	entry, i));
+			i++;
+		}
 		
 	}
 	
